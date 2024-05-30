cb4efb9dccd65668dbde19a791ffee8df6bf95d6
DaveMadridRomero <davemadrid267@gmail.com>
2024-05-29 19:55:27 -0500

    Cambios generales

--- ahorcado.py
+++ ahorcado.py
@@ -1,6 +1,6 @@
-#importar la biblioteca random para que la computadora seleccione una palabra de mi lista
+#importar la biblioteca random para que la computadora seleccione cualquier palabra de mi lista
 import random
-
+print("Desarrollador: Dave Madrid Romero")
 print("================================")
 print("Bienvenido al juego del ahorcado")
 print("================================")
@@ -11,20 +11,9 @@ palabra_generada = random.choice(lista_de_palabras)
 posiciones = "_ " * len(palabra_generada)
 #Definir las oportunidades que tiene el jugador
 vidas = 6
-#letra1 = str(input("Por favor introduce una letra: "))[:1]
-#Bucles y condicional para introducir y detectar si las letras son correctas o no
-while True:
-    print(posiciones)
-    letra = str(input("Por favor introduce una letra: "))[:1]
-    #En caso de introducir una mayúscula el programa lo convierte en minúscula
-    letra.lower 
-    if letra in palabra_generada:
-        #definir una iteración con la cantidad de caracteres que tiene la palabra elegida por la máquina
-        for i in range(len(palabra_generada)):
-            if palabra_generada[i] == letra:
-                posiciones = posiciones[:i] + letra + posiciones[i+1]
-                           
-            
-
+letra1 = str(input("Por favor introduce una letra: "))[:1]
+if letra1 in palabra_generada:
     
 
+
+    
