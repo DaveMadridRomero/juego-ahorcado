882d460cff18d14714ecd2847416b9f7e20468bc
DaveMadridRomero <davemadrid267@gmail.com>
2024-06-13 16:33:51 -0500

    Arreglos

--- ahorcado.py
+++ ahorcado.py
@@ -10,10 +10,10 @@ print("Bienvenido al juego del ahorcado")
 print("---------------------------------")
 lista_de_dificultad = "nada"
 #Dar la opción de jugar dos modos, un jugador contra la máquina o dos jugadores con otra persona
-modo = int(input("Pulsa el número 1 para el modo de un jugador o pulsa 2 para el modo 2 jugadores: "))
+modo = int(input("1)Modo 1 jugador \n2)Modo 2 jugadores \n\nEscoja una opción: "))
 while modo <1 or modo >2:
     print("El número ingresado no es permitido, vuelve a elegir el número que corresponda al modo de juego ")
-    modo = int(input("Pulsa el número 1 para el modo de un jugador o pulsa 2 para el modo 2 jugadores: "))
+    modo = int(input("1) Modo 1 jugador \n2) Modo 2 jugadores \n\nEscoja una opción: "))
 if modo == 1:
     #Defino mi propia función para la selección de dificultad del juego, la dificultad se define por la longitud de las palabras
     print("Has elegido el modo de un jugador")
@@ -91,7 +91,7 @@ if modo == 2:
     nombre1 = input("Ingrese su nombre jugador 1: ")
     nombre2 = input("Ingrese su nombre Jugador 2: ")
     # El primer jugador ingresa su palabra
-    PalabraJ1 = stdiomask.getpass(prompt= f"{nombre1} ingrese la palabra para que lo adivine el jugador {nombre2} ")
+    PalabraJ1 = stdiomask.getpass(prompt= f"{nombre1} ingresa la palabra para que la adivine el jugador {nombre2} ")
     PalabraJ1 = PalabraJ1.lower()
     # Se muestra las posiciones de la palabra del primer jugador
     letras_introducidas = []
@@ -100,7 +100,7 @@ if modo == 2:
     # Se empieza con 0 vidas
     vidas = 0
     while vidas <5:
-        letra = input("Por favor introduce una letra: ")[:1]
+        letra = input("Por favor ingresa una letra: ")[:1]
         letra = letra.lower()
         if letra in letras_introducidas:
             print("Esa letra ya fue adivida, prueba con otra")
@@ -110,7 +110,7 @@ if modo == 2:
                     posiciones = posiciones[:i] + letra + posiciones[i+1:]
             print("¡Bien hecho!",nombre2, "la palabra es:", posiciones)
             if "-" not in posiciones:
-                print("Felicidades ",nombre2, "adivinaste la palabra:", posiciones)
+                print("Felicidades ",nombre2, "adivinaste la palabra:", posiciones.upper())
                 break
         else: 
         #En caso de fallar se le agregara 1 a su contador de vidas hasta llegar al máximo de 5
@@ -135,7 +135,7 @@ if modo == 2:
                 print("No adivinaste la palabra ",nombre1, ".La palabra correcta era", PalabraJ1)
                 break
     # El primer jugador ingresa su palabra
-    PalabraJ2 = stdiomask.getpass(prompt= f"{nombre1} ingrese la palabra para que lo adivine el jugador {nombre2} ")
+    PalabraJ2 = stdiomask.getpass(prompt= f"{nombre1} ingrese la palabra para que lo adivine el jugador {nombre2}: ")
     PalabraJ2 = PalabraJ2.lower()
     # Se muestra las posiciones de la palabra del segundo jugador
     letras_introducidas = []
@@ -154,7 +154,7 @@ if modo == 2:
                         posiciones = posiciones[:i] + letra + posiciones[i+1:]
                 print("¡Bien hecho!",nombre1, "la palabra es:", posiciones)
                 if "-" not in posiciones:
-                    print("Felicidades ", nombre1, "adivinaste la palabra:", posiciones)
+                    print("Felicidades ", nombre1, "adivinaste la palabra:", posiciones.upper())
                     break
             else: 
             #En caso de fallar se le agregara 1 a su contador de vidas hasta llegar al máximo de 5
